06abf9efb4b6a707d31f383bce4dc52999b77585
Tuchau14112004 <22070310@vnu.edu.vn>
2026-03-10 00:20:38 +0700

    Create ex 2.php

---
+++ SS2/Part 4/ex 2.php
@@ -0,0 +1,21 @@
+<?php
+
+$students = [
+    ['name' => 'Alice', 'grade' => 90],
+    ['name' => 'Bob', 'grade' => 85],
+    ['name' => 'Charlie', 'grade' => 92]
+];
+
+echo "<table border='1'>";
+echo "<tr><th>Name</th><th>Grade</th></tr>";
+
+foreach ($students as $student) {
+    echo "<tr>";
+    echo "<td>" . $student['name'] . "</td>";
+    echo "<td>" . $student['grade'] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+?>
